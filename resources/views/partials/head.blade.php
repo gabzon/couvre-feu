fb2aa216ccd7ef98eb722ee91793290926ff0fda
gabzon <gab.zambrano@gmail.com>
2017-07-21 23:55:05 -0500

    git ignore updated

--- resources/views/partials/head.blade.php
+++
@@ -1,7 +0,0 @@
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  @php(wp_head())
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.1.20/jquery.fancybox.min.css">
-</head>
